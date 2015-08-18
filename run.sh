b076829a29fe2a7b32bd75ddde4816883bcbaa3f
alberto <alberto@tutum.co>
2015-08-18 10:25:34 +0200

    List packages that require reboot

--- run.sh
+++ run.sh
@@ -2,32 +2,41 @@
 # See more file distros at http://linuxmafia.com/faq/Admin/release-files.html
 
 if [ -r "/etc/lsb-release" ]; then
-    echo "Detected 'lsb-release' (checking debian release...)"
+    echo ">>> Detected 'lsb-release' (checking debian release...)"
     DISTRO_ID=$(grep 'DISTRIB_ID' /etc/lsb-release | sed 's/DISTRIB_ID=//')
     DISTRO_RELEASE=$(grep 'DISTRIB_RELEASE' /etc/lsb-release | sed 's/DISTRIB_RELEASE=//')
     DISTRO="$DISTRO_ID $DISTRO_RELEASE"
-    echo "Distribution: $DISTRO"
+    echo ">>> Distribution: $DISTRO"
     if [ "${DISTRO}" = "Ubuntu 14.04" ]; then
-        echo "Distribution $DISTRO supported! upgrading..."
         export DEBIAN_FRONTEND=noninteractive
+        echo ">>> Distribution $DISTRO supported! Updating apt..."
         apt-get -y update
+        echo ">>>"
         /usr/lib/update-notifier/apt-check --human-readable
+        echo ">>>"
         grep security /etc/apt/sources.list > /tmp/security.list
+        echo ">>> Using the following source list:"
+        cat /tmp/security.list
+        echo ">>> Upgrading..."
         apt-get upgrade -y -oDir::Etc::Sourcelist=/tmp/security.list
         RET=$?
         if [ $RET -eq 0 ]; then
-            echo "Security upgrade finished"
-            echo "Checking reboot required..."
+            echo ">>> Security upgrade finished! Checking reboot required..."
             if [ -f /var/run/reboot-required ]; then
-                echo "Reboot required!"
+                echo ">>> Reboot required!"
+                if [ -f /var/run/reboot-required.pkgs ]; then
+                    cat /var/run/reboot-required.pkgs
+                fi
             else
-                echo "Reboot not required"
+                echo ">>> Reboot not required"
             fi
         else
-            echo "Security upgrade failed with error code $RET"
+            echo ">>> Security upgrade failed with error code $RET"
         fi
         rm /tmp/security.list
+        echo ">>>"
         /usr/lib/update-notifier/apt-check --human-readable
+        echo ">>>"
     else
         echo "Ubuntu distribution not supported."
     fi
